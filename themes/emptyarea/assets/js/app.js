393f7b12b32e0f878a3402a55f31a956cdc506db
taoguangc <taoguangc@gmail.com>
2024-10-19 23:01:21 +0800

    update menu offcanvas

--- themes/emptyarea/assets/js/app.js
+++ themes/emptyarea/assets/js/app.js
@@ -1,10 +1,6 @@
 import Lenis from 'lenis'
 const lenis = new Lenis()
 
-lenis.on('scroll', e => {
-  console.log(e)
-})
-
 function raf(time) {
   lenis.raf(time)
   requestAnimationFrame(raf)
@@ -18,3 +14,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     lenis.scrollTo(this.getAttribute('href'))
   })
 })
+
+const headerToggle = document.querySelector('.header__toggle')
+const headerMenu = document.querySelector('.header__menu')
+if (headerToggle && headerMenu) {
+  headerToggle.addEventListener('click', () => {
+    headerToggle.classList.toggle('header__toggle--open')
+    const isExpanded = headerToggle.getAttribute('aria-expanded') === 'true'
+    const newExpandedValue = isExpanded ? 'false' : 'true'
+    headerToggle.setAttribute('aria-expanded', newExpandedValue)
+    headerMenu.classList.toggle('header__menu--open')
+  })
+}
